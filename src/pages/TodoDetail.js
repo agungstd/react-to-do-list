419651eda08688015983ccd5699586907dd28bd3
agungstd <apakatadunia11@gmail.com>
2025-02-18 12:27:01 +0700

    Update Todo Detail

--- src/pages/TodoDetail.js
+++ src/pages/TodoDetail.js
@@ -1,26 +1,21 @@
 import React, { lazy, Suspense, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 import { titlePage } from "../lib/titleHead";
 import { Creators as TodoActions } from "../redux/TodoRedux";
-const TodoDetailModule = lazy(() =>
-  import("../components/TodoDetail/TodoDetailModule")
-);
-const Header = lazy(() =>
-  import("../layout/Header")
-);
+
+const TodoDetailModule = lazy(() => import("../components/TodoDetail/TodoDetailModule"));
+const Header = lazy(() => import("../layout/Header"));
 
 function TodoDetail() {
-  const params = useParams().todoId
-  const dispatch = useDispatch()
-  const getTodoDetail = (data) => dispatch(TodoActions.getActivityDetailRequest(data))
+  const { todoId } = useParams();
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    titlePage({
-      title: "To Do List - Detail",
-    });
-    getTodoDetail(params)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    titlePage({ title: "To Do List - Detail" });
+    dispatch(TodoActions.getActivityDetailRequest(todoId));
+  }, [dispatch, todoId]);
+
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <Header />
